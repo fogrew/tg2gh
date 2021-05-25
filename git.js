326d5f987f8c80ba134721dbf60149c3a1f5b5e9
Andrew Gurylev <andrew@gurylev.com>
2021-05-25 21:35:40 +0300

    feat: add FILE_EXTENSION option

--- git.js
+++ git.js
@@ -4,9 +4,14 @@ const fs = require('fs').promises
 const path = require('path')
 
 module.exports = async function (text) {
-  const { BLOG_PATH, COMMIT_MESSAGE } = process.env || '.'
+  const {
+    BLOG_PATH,
+    COMMIT_MESSAGE,
+    FILE_EXTENSION,
+    GIT_BRANCH_NAME
+  } = process.env
   const DATE_MASK = DateTime.now().toString()
-  const FILE_NAME = `${DATE_MASK}.md`
+  const FILE_NAME = `${DATE_MASK}${FILE_EXTENSION}`
   const FILE_PATH = path.normalize(`${BLOG_PATH}/${FILE_NAME}`)
 
   const git = simpleGit(BLOG_PATH)
